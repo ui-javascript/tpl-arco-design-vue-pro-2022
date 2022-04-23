d2730cede72bb7c547aee8866309e2fa8a0660da
luo0412 <1095847440@qq.com>
2022-04-24 01:28:26 +0800

    feat: mock adjust

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,24 +1,9 @@
 import Mock from 'mockjs';
 
-import './user';
-import './message-box';
-
-import '@/views/dashboard/workplace/mock';
-
-import '@/views/dashboard/monitor/mock';
-
-import '@/views/list/card/mock';
-import '@/views/list/search-table/mock';
-
-import '@/views/form/step/mock';
-
-import '@/views/profile/basic/mock';
-
-import '@/views/visualization/data-analysis/mock';
-import '@/views/visualization/multi-dimension-data-analysis/mock';
-
-import '@/views/user/info/mock';
-import '@/views/user/setting/mock';
+const modules = import.meta.glob('../{views,components}/**/*mock.ts');
+Object.keys(modules).forEach((key) => {
+  modules[key]();
+});
 
 Mock.setup({
   timeout: '600-1000',
