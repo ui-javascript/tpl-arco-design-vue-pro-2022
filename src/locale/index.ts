4e553a01b5b8bc6830c087c35ea27dd35600dbca
luo0412 <1095847440@qq.com>
2022-04-24 00:38:38 +0800

    feat: adjust

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,6 +1,6 @@
 import { createI18n } from 'vue-i18n';
-import en from './locales/en-US';
-import cn from './locales/zh-CN';
+import en from './modules/en-US';
+import cn from './modules/zh-CN';
 
 export const LOCALE_OPTIONS = [
   { label: '中文', value: 'zh-CN' },
